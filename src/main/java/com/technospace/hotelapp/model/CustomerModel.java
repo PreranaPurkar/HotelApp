a4d80b6a7956582f8c26b2bbc9d00ed5f6ffb59c
PreranaPurkar <preranapurkar12@gmail.com>
2023-07-06 15:31:38 +0530

    [MODIFIED]:[Prerana] Model for Customer

---
+++ src/main/java/com/technospace/hotelapp/model/CustomerModel.java
@@ -0,0 +1,32 @@
+package com.technospace.hotelapp.model;
+
+public class CustomerModel {
+	private int cid;
+	private String name;
+	private String mobileno;
+	private String emailid;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(String mobileno) {
+		this.mobileno = mobileno;
+	}
+	public String getEmailid() {
+		return emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+}
